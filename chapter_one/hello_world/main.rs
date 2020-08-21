a944be5b94d963a7e6111ff0e9bfd27133650988
Max Huebler <max@huebler.us>
2020-08-21 01:59:00 -0400

    Initial commit

---
+++ chapter_one/hello_world/main.rs
@@ -0,0 +1,9 @@
+// https://doc.rust-lang.org/book/ch01-02-hello-world.html
+// Rust is an ahead-of-time compiled language, like C and C++
+
+// We can enforce code formatting by running the command: rustfmt main.rs
+fn main() {
+    // Rust indents with four spaces
+    // println! calls a Rust macro, "!" means we're calling a macro instead of a function
+    println!("Hello, world!");
+}
