e5e22c99652a3f3770734ec77bb9af3b13d84348
Max Huebler <max@huebler.us>
2020-08-21 02:11:03 -0400

    Chapter 1 Section 03 (hello_cargo)

--- chapter_one/hello_cargo/src/main.rs
+++ chapter_one/hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
+// https://doc.rust-lang.org/book/ch01-03-hello-cargo.html
+// Cargo expects source files to live inside the src directory
+// After running "cargo build", we can run the executable with "./target/debug/hello_cargo"
+// However, we can simply use the "cargo run" command to combine both of these steps
+
+// We can also use "cargo check" to make sure the code compiles without producing an executable.
+
+// When your project is finally ready for release, you can use cargo build --release to compile it with optimizations.
+// This makes an executable in target/release instead of target/debug
 fn main() {
     println!("Hello, world!");
 }
